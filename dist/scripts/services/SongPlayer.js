120933fee3fb0e910d3c6797e353d05cf472026e
Brian Sung <bksung0708@gmail.com>
2017-04-08 04:08:51 -0700

    assignment 7 completed

--- dist/scripts/services/SongPlayer.js
+++ dist/scripts/services/SongPlayer.js
@@ -1,7 +1,10 @@
  (function() {
      function SongPlayer() {
         var SongPlayer = {};
-
+        /**
+        * @desc content of currently playing song
+        * @type {Object}
+        */
         var currentSong = null;
         /**
         * @desc Buzz object audio file
@@ -27,19 +30,37 @@
  
             currentSong = song;
         };
-
+        /**
+        * @function playSong
+        * @desc plays new song
+        * @param {Object} song
+        */
+        var playSong = function(song) {
+            if (currentBuzzObject) {
+                currentBuzzObject.play();
+                song.playing = true;
+            }
+        };
+        /**
+        * @function SongPlayer.play
+        * @desc plays song with two conditions. #1: when different song was clicked #2: when same song was clicked
+        * @param {Object} song
+        */
         SongPlayer.play = function(song) {
             if (currentSong !== song) {
                 setSong(song);
-                currentBuzzObject.play();   
-                song.playing = true;
+                playSong(song);
             } else if (currentSong === song) {
                 if (currentBuzzObject.isPaused()) {
-                    currentBuzzObject.play();
+                    playSong(song);
                 }
             }
         };
-         
+        /**
+        * @function SongPlayer.pause
+        * @desc pauses currently playing song
+        * @param {Object} song
+        */
         SongPlayer.pause = function(song) {
             currentBuzzObject.pause();
             song.playing = false;

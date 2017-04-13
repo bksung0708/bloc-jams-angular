2e6ab2554faf3f82c07780e1a40a144a2ce1210e
Brian Sung <bksung0708@gmail.com>
2017-04-13 15:46:50 -0700

    assignment 9 completed

--- dist/scripts/directives/seekBar.js
+++ dist/scripts/directives/seekBar.js
@@ -29,6 +29,11 @@
                  scope.fillStyle = function() {
                      return {width: percentString()};
                  };
+                 
+                 scope.thumbStyle = function() {
+                     return {left: percentString()};
+                 };
+                 
                  scope.onClickSeekBar = function(event) {
                      var percent = calculatePercent(seekBar, event);
                      scope.value = percent * scope.max;
@@ -37,9 +42,7 @@
                  scope.trackThumb = function() {
                      $document.bind('mousemove.thumb', function(event) {
                          var percent = calculatePercent(seekBar, event);
-                         scope.$apply(function() {
                              scope.value = percent * scope.max;
-                         });
                      });
 
                      $document.bind('mouseup.thumb', function() {
